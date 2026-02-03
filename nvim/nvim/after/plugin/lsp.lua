a0dfc7fd37c9a3985faf287239732d314767ea31
dacianbarbu <dacianbarbu@waterford.org>
2026-02-03 14:46:48 +0200

    Updates lsp

--- nvim/nvim/after/plugin/lsp.lua
+++ nvim/nvim/after/plugin/lsp.lua
@@ -1,43 +1,75 @@
-local lsp_zero = require('lsp-zero')
 local mason = require('mason')
-local mason_lspconfig = require('mason-lspconfig')
+-- local mason_lspconfig = require('mason-lspconfig')
 local cmp = require('cmp')
 
-lsp_zero.preset("recommended")
+mason.setup()
+-- mason_lspconfig.setup({
+--   ensure_installed = {
+--     "ts_ls",
+--     "bashls",
+--     "gopls",
+--   },
+--   automatic_installation = true,
+-- })
 
+local on_attach = function(_, bufnr)
+    local opts = { buffer = bufnr, silent = true }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gu", function() require('telescope.builtin').lsp_references({ initial_mode = 'normal'}) end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set("n", "ga", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("v", "ga", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "gf", function() vim.lsp.buf.format() end, opts)
+end
 
-mason.setup({})
-mason_lspconfig.setup({
-	ensure_installed = {
-		'tsserver',
-		"bashls",
-		"gopls",
-	},
-	handlers = {
-		lsp_zero.default_setup,
-	}
-})
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-lsp_zero.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "gu", function() require('telescope.builtin').lsp_references({ initial_mode = 'normal'}) end, opts)
-	vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-	vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
-	vim.keymap.set("n", "ga", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("v", "ga", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "gr", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("n", "gf", function() vim.lsp.buf.format() end, opts)
-end)
-
+-- cmp.setup({
+-- 	preselect = 'item',
+-- 	completion = {
+-- 		completeopt = 'menu,menuone,noinsert'
+-- 	},
+-- 	mapping = cmp.mapping.preset.insert({
+-- 		['<CR>'] = cmp.mapping.confirm({select = false}),
+-- 	}),
+-- })
+--
 cmp.setup({
-	preselect = 'item',
-	completion = {
-		completeopt = 'menu,menuone,noinsert'
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<CR>'] = cmp.mapping.confirm({select = false}),
-	}),
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp', max_item_count = 10 },
+    }),
 })
 
-lsp_zero.setup()
+vim.lsp.config('ts-server', {
+    cmd = { "typescript-language-server", "--stdio" },
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "json" },
+    root_dir = vim.fs.root(0, { "tsconfig.json", "package.json", ".git" }),
+    on_attach = on_attach,
+    settings = {},
+    capabilities = capabilities,
+})
+
+vim.lsp.config('jsonls', {
+  cmd = { "npx", "vscode-json-languageserver", "--stdio" },
+  filetypes = { "json", "jsonc" },
+  root_dir = vim.fs.root(0, { ".git", "tsconfig.json" }),
+  on_attach = on_attach,
+  settings = {
+    json = {
+      validate = { enable = true },
+      schemas = {},
+    },
+  },
+  capabilities = capabilities,
+})
+
+vim.lsp.enable({'ts-server', 'jsonls' })
