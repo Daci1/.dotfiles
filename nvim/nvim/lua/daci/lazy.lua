b809646d6b51146339a4cda3a683c5d1cf0773c5
dacianbarbu <dacianbarbu@waterford.org>
2026-02-03 14:47:13 +0200

    Updates/adds dependencies

--- nvim/nvim/lua/daci/lazy.lua
+++ nvim/nvim/lua/daci/lazy.lua
@@ -14,12 +14,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                              , branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
-	{
-		"catppuccin/nvim", name = "catppuccin", priority = 1000 
-	},
+    { "nickkadutskyi/jb.nvim" },
 	{
 		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
 	},
@@ -30,13 +27,16 @@ require("lazy").setup({
 		"tpope/vim-fugitive"
 	},
 	{
+		"lewis6991/gitsigns.nvim"
+	},
+	{
 		'williamboman/mason.nvim'
 	},
 	{
 		'williamboman/mason-lspconfig.nvim'
 	},
 	{
-		'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'
+		'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', enabled = false,
 	},
 	{
 		'neovim/nvim-lspconfig'
@@ -47,21 +47,78 @@ require("lazy").setup({
 	{
 		'hrsh7th/nvim-cmp'
 	},
-	{
-		'L3MON4D3/LuaSnip'
-	},
 	{ 'numToStr/Comment.nvim', opts = {} },
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
 			"leoluz/nvim-dap-go", -- requires GOPATH to be set correctly and also 'delve' installed
 			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
             "nvim-neotest/nvim-nio",
+            "mxsdev/nvim-dap-vscode-js",
+            -- {
+            --     "microsoft/vscode-js-debug",
+            --     build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+            --     version = "1.*",
+            -- },
+            {
+                "microsoft/vscode-js-debug",
+                version = "1.97.1",
+                build = "npm i && npx gulp dapDebugServer"
+            }
 		}
 	},
     {
         'stevearc/dressing.nvim',
         opts = {},
+    },
+    {
+        "yetone/avante.nvim",
+        build = "make",
+        event = "VeryLazy",
+        version = false, -- Never set this value to "*"! Never!
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            {
+                -- Make sure to set this up properly if you have lazy=true
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        }
+    },
+    {
+        "klen/nvim-test",
+        config = function()
+            require("nvim-test").setup()
+            require('nvim-test.runners.jest'):setup {
+                -- command = "~/node_modules/.bin/jest",
+                args = {
+                    "--config", "jest.unit.config.ts",
+                },
+                env = {
+                    NODE_OPTIONS = "--experimental-vm-modules",
+                },
+                -- file_pattern = "\\v(__tests__/.*|(spec|test))\\.(js|jsx|coffee|ts|tsx)$",
+                -- find_files = { "{name}.test.{ext}", "{name}.spec.{ext}" },
+                filename_modifier = nil,
+                working_directory = nil,
+            }
+        end,
     }
 })
